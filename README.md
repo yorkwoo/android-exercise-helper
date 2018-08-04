caffe5db6fd825904edc755994bc7b2f5039cc43
Yungkang Wu <gkgwpk@yahoo.com.tw>
2018-08-04 17:02:05 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Another app to work with main exercise.
+
---
+++ app/src/main/java/yorkwusoft/com/logincheck/ykLoginCheck.java
@@ -0,0 +1,13 @@
+package yorkwusoft.com.logincheck;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ykLoginCheck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_check);
+    }
+}
